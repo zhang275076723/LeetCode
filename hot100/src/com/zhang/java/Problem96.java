18ab0262d24f1f50912c30611fbc2b01423193c9
zsy <275076723@qq.com>
2022-05-01 09:58:41 +0800

    96、98

---
+++ hot100/src/com/zhang/java/Problem96.java
@@ -0,0 +1,67 @@
+package com.zhang.java;
+
+/**
+ * @Date 2022/4/30 9:26
+ * @Author zsy
+ * @Description 给你一个整数 n ，求恰由 n 个节点组成且节点值从 1 到 n 互不相同的 二叉搜索树 有多少种？
+ * 返回满足题意的二叉搜索树的种数。
+ * <p>
+ * 输入：n = 3
+ * 输出：5
+ * <p>
+ * 输入：n = 1
+ * 输出：1
+ * <p>
+ * 1 <= n <= 19
+ */
+public class Problem96 {
+    public static void main(String[] args) {
+        Problem96 problem96 = new Problem96();
+        System.out.println(problem96.numTrees(4));
+        System.out.println(problem96.numTrees2(4));
+    }
+
+    /**
+     * 动态规划，时间复杂度O(n^2)，空间复杂度O(n)
+     * dp[n]：n个节点二叉搜索树的个数
+     * 当1为根节点时，左子树有0个节点，右子树有n-1个节点，此时二叉搜索树的个数为dp[0]*dp[n-1]
+     * 当2为根节点时，左子树有1个节点，右子树有n-2个节点，此时二叉搜索树的个数为dp[1]*dp[n-2]
+     * ...
+     * 当n为根节点时，左子树有n-1个节点，右子树有0个节点，此时二叉搜索树的个数为dp[n-1]*dp[0]
+     * dp[n] = Σ(i=0到n-1)dp[i]*dp[n-1-i]
+     *
+     * @param n
+     * @return
+     */
+    public int numTrees(int n) {
+        int[] dp = new int[n + 1];
+        dp[0] = 1;
+        dp[1] = 1;
+
+        for (int i = 2; i <= n; i++) {
+            for (int j = 0; j <= i - 1; j++) {
+                dp[i] = dp[i] + dp[j] * dp[i - 1 - j];
+            }
+        }
+        return dp[n];
+    }
+
+    /**
+     * Catalan数，时间复杂度O(n)，空间复杂度O(1)
+     * Cn+1 = 2(2n+1)/(n+2) * Cn
+     * Cn = (2n,n)/(n+1) (2n,n)是2n中取n个的组合数
+     *
+     * @param n
+     * @return
+     */
+    public int numTrees2(int n) {
+        //使用long，避免乘法时溢出
+        long result = 1;
+        for (int i = 0; i < n; i++) {
+            //除以i+1，则能保证整除；如果除以n-i，则不能保证整除
+            result = result * (2L * n - i) / (i + 1);
+        }
+        result = result / (n + 1);
+        return (int) result;
+    }
+}
---
+++ hot100/src/com/zhang/java/Problem98.java
@@ -0,0 +1,167 @@
+package com.zhang.java;
+
+import java.util.*;
+
+/**
+ * @Date 2022/4/30 11:39
+ * @Author zsy
+ * @Description 给你一个二叉树的根节点 root ，判断其是否是一个有效的二叉搜索树。
+ * <p>
+ * 有效 二叉搜索树定义如下：
+ * 节点的左子树只包含 小于 当前节点的数。
+ * 节点的右子树只包含 大于 当前节点的数。
+ * 所有左子树和右子树自身必须也是二叉搜索树。
+ * <p>
+ * 输入：root = [2,1,3]
+ * 输出：true
+ * <p>
+ * 输入：root = [5,1,4,null,null,3,6]
+ * 输出：false
+ * 解释：根节点的值是 5 ，但是右子节点的值是 4 。
+ * <p>
+ * 树中节点数目范围在[1, 10^4] 内
+ * -2^31 <= Node.val <= 2^31 - 1
+ */
+public class Problem98 {
+    //中序遍历中当前节点的上一个节点值，使用long是因为节点值的范围在int，
+    //且能取到最小的int，所以要使用long取到比最小的int更小的值作为初始值
+    private long preNodeValue = Long.MIN_VALUE;
+
+    public static void main(String[] args) {
+        Problem98 problem98 = new Problem98();
+//        String[] data = {"5", "1", "4", "null", "null", "3", "6"};
+        String[] data = {"5", "4", "6", "null", "null", "3", "7"};
+//        TreeNode root = problem98.buildTree(data);
+        TreeNode root = new TreeNode(1);
+        TreeNode node2 = new TreeNode(1);
+        root.left = node2;
+
+        System.out.println(problem98.isValidBST(root));
+        System.out.println(problem98.isValidBST2(root));
+    }
+
+    /**
+     * 递归中序遍历，时间复杂度O(n)，空间复杂度O(n)
+     *
+     * @param root
+     * @return
+     */
+    public boolean isValidBST(TreeNode root) {
+        if (root == null) {
+            return true;
+        }
+
+        return inorder(root);
+    }
+
+    /**
+     * 非递归中序遍历，时间复杂度O(n)，空间复杂度O(n)
+     *
+     * @param root
+     * @return
+     */
+    public boolean isValidBST2(TreeNode root) {
+        if (root == null) {
+            return true;
+        }
+
+        Stack<TreeNode> stack = new Stack<>();
+        TreeNode node = root;
+        //中序遍历中当前节点的上一个节点值，使用long是因为节点值的范围在int，
+        //且能取到最小的int，所以要使用long取到比最小的int更小的值作为初始值
+        long preNodeValue = Long.MIN_VALUE;
+        while (node != null || !stack.isEmpty()) {
+            if (node != null) {
+                stack.push(node);
+                node = node.left;
+            } else {
+                node = stack.pop();
+                //不满足二叉搜索树要求，直接返回false
+                if (node.val <= preNodeValue) {
+                    return false;
+                }
+                preNodeValue = node.val;
+                node = node.right;
+            }
+        }
+        return true;
+    }
+
+    private boolean inorder(TreeNode root) {
+        if (root == null) {
+            return true;
+        }
+
+        //遍历左子树
+        boolean leftFlag = inorder(root.left);
+        if (!leftFlag) {
+            return false;
+        }
+
+        if (root.val <= preNodeValue) {
+            return false;
+        }
+        preNodeValue = root.val;
+
+        //遍历右子树
+        boolean rightFlag = inorder(root.right);
+        return rightFlag;
+    }
+
+    /**
+     * 建树
+     * @param data
+     * @return
+     */
+    private TreeNode buildTree(String[] data) {
+        if (data == null || data.length == 0) {
+            return null;
+        }
+
+        List<String> list = new ArrayList<>(Arrays.asList(data));
+        Queue<TreeNode> queue = new LinkedList<>();
+        TreeNode root = new TreeNode(Integer.parseInt(list.remove(0)));
+        queue.offer(root);
+
+        while (!queue.isEmpty()) {
+            TreeNode node = queue.poll();
+            if (!list.isEmpty()) {
+                String leftNodeValue = list.remove(0);
+                if (!"null".equals(leftNodeValue)) {
+                    TreeNode leftNode = new TreeNode(Integer.parseInt(leftNodeValue));
+                    node.left = leftNode;
+                    queue.offer(leftNode);
+                }
+            }
+            if (!list.isEmpty()) {
+                String rightNodeValue = list.remove(0);
+                if (!"null".equals(rightNodeValue)) {
+                    TreeNode rightNode = new TreeNode(Integer.parseInt(rightNodeValue));
+                    node.right = rightNode;
+                    queue.offer(rightNode);
+                }
+            }
+        }
+
+        return root;
+    }
+
+    public static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+}
