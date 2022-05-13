73d1cb56a4c1fb4161c0db3a5847e50438d80d84
zsy <275076723@qq.com>
2022-05-13 11:14:47 +0800

    206、207、575

---
+++ hot100/src/com/zhang/java/Problem206.java
@@ -0,0 +1,107 @@
+package com.zhang.java;
+
+/**
+ * @Date 2022/5/13 8:48
+ * @Author zsy
+ * @Description 反转链表
+ * 给你单链表的头节点 head ，请你反转链表，并返回反转后的链表。
+ * <p>
+ * 输入：head = [1,2,3,4,5]
+ * 输出：[5,4,3,2,1]
+ * <p>
+ * 输入：head = [1,2]
+ * 输出：[2,1]
+ * <p>
+ * 输入：head = []
+ * 输出：[]
+ * <p>
+ * 链表中节点的数目范围是 [0, 5000]
+ * -5000 <= Node.val <= 5000
+ */
+public class Problem206 {
+    public static void main(String[] args) {
+        Problem206 problem206 = new Problem206();
+        int[] data = {1, 2, 3, 4, 5};
+        ListNode head = problem206.buildLinkedList(data);
+//        head = problem206.reverseList(head);
+        head = problem206.reverseList2(head);
+        while (head != null) {
+            System.out.println(head.val);
+            head = head.next;
+        }
+    }
+
+    /**
+     * 非递归反转链表
+     * 时间复杂度O(n)，空间复杂度O(1)
+     *
+     * @param head
+     * @return
+     */
+    public ListNode reverseList(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+
+        ListNode node = head;
+        ListNode nextNode = node.next;
+        ListNode preNode = null;
+        while (nextNode != null) {
+            node.next = preNode;
+            preNode = node;
+            node = nextNode;
+            nextNode = nextNode.next;
+        }
+        node.next = preNode;
+        return node;
+    }
+
+    /**
+     * 递归反转链表
+     * 时间复杂度O(n)，空间复杂度O(n)
+     *
+     * @param head
+     * @return
+     */
+    public ListNode reverseList2(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+
+        ListNode node = reverseList2(head.next);
+        head.next.next = head;
+        head.next = null;
+        return node;
+    }
+
+    private ListNode buildLinkedList(int[] data) {
+        if (data == null || data.length == 0) {
+            return null;
+        }
+
+        ListNode head = new ListNode(data[0]);
+        ListNode node = head;
+        for (int i = 1; i < data.length; i++) {
+            node.next = new ListNode(data[i]);
+            node = node.next;
+        }
+        return head;
+    }
+
+    private static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+}
---
+++ hot100/src/com/zhang/java/Problem207.java
@@ -0,0 +1,159 @@
+package com.zhang.java;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+/**
+ * @Date 2022/5/13 9:06
+ * @Author zsy
+ * @Description 课程表 华为机试题
+ * 你这个学期必须选修 numCourses 门课程，记为 0 到 numCourses - 1 。
+ * 在选修某些课程之前需要一些先修课程。
+ * 先修课程按数组 prerequisites 给出，其中 prerequisites[i] = [ai, bi] ，
+ * 表示如果要学习课程 ai 则 必须 先学习课程 bi 。
+ * 例如，先修课程对 [0, 1] 表示：想要学习课程 0 ，你需要先完成课程 1 。
+ * 请你判断是否可能完成所有课程的学习？如果可以，返回 true ；否则，返回 false 。
+ * <p>
+ * 输入：numCourses = 2, prerequisites = [[1,0]]
+ * 输出：true
+ * 解释：总共有 2 门课程。学习课程 1 之前，你需要完成课程 0 。这是可能的。
+ * <p>
+ * 输入：numCourses = 2, prerequisites = [[1,0],[0,1]]
+ * 输出：false
+ * 解释：总共有 2 门课程。学习课程 1 之前，你需要先完成 课程 0 ；并且学习课程 0 之前，你还应先完成课程 1 。这是不可能的。
+ * <p>
+ * 1 <= numCourses <= 105
+ * 0 <= prerequisites.length <= 5000
+ * prerequisites[i].length == 2
+ * 0 <= ai, bi < numCourses
+ * prerequisites[i] 中的所有课程对 互不相同
+ */
+public class Problem207 {
+    /**
+     * dfs中，是否可以完成所有课程
+     */
+    private boolean valid = true;
+
+    public static void main(String[] args) {
+        Problem207 problem207 = new Problem207();
+        int numCourses = 6;
+        int[][] prerequisites = {{3, 0}, {3, 1}, {4, 1}, {4, 2}, {5, 3}, {5, 4}};
+        System.out.println(problem207.canFinish(numCourses, prerequisites));
+        System.out.println(problem207.canFinish2(numCourses, prerequisites));
+    }
+
+    /**
+     * dfs，判断图中是否有拓扑排序
+     * 本质：找出度为0的节点，出度为0的节点在拓扑排序中一定排在后面
+     * 时间复杂度O(m+n)，空间复杂度O(m+n)
+     * 拓扑排序：有向无环图所有顶点进行排序，使图中任意一对顶点u、v，边<u,v>在排序中u出现在v之前
+     *
+     * @param numCourses
+     * @param prerequisites
+     * @return
+     */
+    public boolean canFinish(int numCourses, int[][] prerequisites) {
+        //使用集合存放临界表
+        List<List<Integer>> edges = new ArrayList<>();
+        //节点对应的课访问数组，0-未访问，1-正在访问，2-已访问
+        int[] visited = new int[numCourses];
+
+        for (int i = 0; i < numCourses; i++) {
+            edges.add(new ArrayList<>());
+        }
+        for (int i = 0; i < prerequisites.length; i++) {
+            edges.get(prerequisites[i][1]).add(prerequisites[i][0]);
+        }
+
+        for (int i = 0; i < numCourses; i++) {
+            //不满足当前课程要先上的课程，说明有环，返回false
+            if (!valid) {
+                return false;
+            }
+
+            //当前节点未访问，从当前节点进行dfs
+            if (visited[i] == 0) {
+                dfs(i, visited, edges);
+            }
+        }
+
+        return valid;
+    }
+
+    /**
+     * bfs，判断图中是否有拓扑排序
+     * 时间复杂度O(m+n)，空间复杂度O(m+n)
+     * 本质：找入度为0的节点，入度为0的节点在拓扑排序中一定排在前面
+     * 拓扑排序：有向无环图所有顶点进行排序，使图中任意一对顶点u、v，边<u,v>在排序中u出现在v之前
+     *
+     * @param numCourses
+     * @param prerequisites
+     * @return
+     */
+    public boolean canFinish2(int numCourses, int[][] prerequisites) {
+        //使用集合存放临界表
+        List<List<Integer>> edges = new ArrayList<>();
+        //节点的入度
+        int[] inDegree = new int[numCourses];
+
+        for (int i = 0; i < numCourses; i++) {
+            edges.add(new ArrayList<>());
+        }
+        for (int i = 0; i < prerequisites.length; i++) {
+            edges.get(prerequisites[i][1]).add(prerequisites[i][0]);
+            inDegree[prerequisites[i][0]]++;
+        }
+
+        //存放入度为0的队列
+        Queue<Integer> queue = new LinkedList<>();
+        //能够访问到的入度为0的节点
+        int visited = 0;
+
+        for (int i = 0; i < numCourses; i++) {
+            if (inDegree[i] == 0) {
+                queue.offer(i);
+            }
+        }
+
+        while (!queue.isEmpty()) {
+            visited++;
+            int u = queue.poll();
+            //u的邻接顶点v
+            for (int v : edges.get(u)) {
+                //v的入度减1
+                inDegree[v]--;
+                if (inDegree[v] == 0) {
+                    queue.offer(v);
+                }
+            }
+        }
+
+        //能够访问所有节点，表示没有环
+        return visited == numCourses;
+    }
+
+    private void dfs(int u, int[] visited, List<List<Integer>> edges) {
+        //当前节点正在访问
+        visited[u] = 1;
+
+        //u的邻接顶点v
+        for (int v : edges.get(u)) {
+            //u的邻接顶点v未访问
+            if (visited[v] == 0) {
+                dfs(v, visited, edges);
+                //不满足当前课程要先上的课程，直接返回
+                if (!valid) {
+                    return;
+                }
+            } else if (visited[v] == 1) {//u的邻接顶点正在访问，说明有环，直接返回
+                valid = false;
+                return;
+            }
+        }
+
+        //当前节点已访问
+        visited[u] = 2;
+    }
+}
