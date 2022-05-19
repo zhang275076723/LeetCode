3231638961e1f6661529e6f5f089100d2d7d390b
zsy <275076723@qq.com>
2022-05-19 16:47:13 +0800

    238

---
+++ hot100/src/com/zhang/java/Problem238.java
@@ -0,0 +1,94 @@
+package com.zhang.java;
+
+import java.util.Arrays;
+
+/**
+ * @Date 2022/5/19 15:38
+ * @Author zsy
+ * @Description 除自身以外数组的乘积
+ * 给你一个整数数组 nums，返回 数组 answer ，其中 answer[i] 等于 nums 中除 nums[i] 之外其余各元素的乘积 。
+ * 题目数据 保证 数组 nums之中任意元素的全部前缀元素和后缀的乘积都在 32 位 整数范围内。
+ * 请不要使用除法，且在 O(n) 时间复杂度内完成此题。
+ * <p>
+ * 输入: nums = [1,2,3,4]
+ * 输出: [24,12,8,6]
+ * <p>
+ * 输入: nums = [-1,1,0,-3,3]
+ * 输出: [0,0,9,0,0]
+ * <p>
+ * 2 <= nums.length <= 10^5
+ * -30 <= nums[i] <= 30
+ * 保证 数组 nums之中任意元素的全部前缀元素和后缀的乘积都在  32 位 整数范围内
+ */
+public class Problem238 {
+    public static void main(String[] args) {
+        Problem238 problem238 = new Problem238();
+        int[] nums = {1, 2, 3, 4};
+        int[] result = problem238.productExceptSelf(nums);
+        int[] result2 = problem238.productExceptSelf2(nums);
+        System.out.println(Arrays.toString(result));
+        System.out.println(Arrays.toString(result2));
+    }
+
+    /**
+     * 使用两个前缀数组，一个表示从左边到当前位置前一个位置的乘积，另一个表示从右边到当前位置后一个位置的乘积
+     * 结果数组是两个前缀数组的乘积
+     * 时间复杂度O(n)，空间复杂度O(n)
+     *
+     * @param nums
+     * @return
+     */
+    public int[] productExceptSelf(int[] nums) {
+        if (nums == null || nums.length == 0 || nums.length == 1) {
+            return nums;
+        }
+
+        int[] result = new int[nums.length];
+        int[] left = new int[nums.length];
+        int[] right = new int[nums.length];
+
+        left[0] = 1;
+        right[nums.length - 1] = 1;
+        for (int i = 1; i < nums.length; i++) {
+            left[i] = left[i - 1] * nums[i - 1];
+            right[nums.length - i - 1] = right[nums.length - i] * nums[nums.length - i];
+        }
+
+        for (int i = 0; i < nums.length; i++) {
+            result[i] = left[i] * right[i];
+        }
+
+        return result;
+    }
+
+    /**
+     * 使用两个前缀值，一个表示从左边到当前位置的乘积，另一个表示从右边到当前位置的乘积
+     * 结果数组是两个前缀数组的乘积
+     * 时间复杂度O(n)，空间复杂度O(1)
+     *
+     * @param nums
+     * @return
+     */
+    public int[] productExceptSelf2(int[] nums) {
+        if (nums == null || nums.length == 0 || nums.length == 1) {
+            return nums;
+        }
+
+        int[] result = new int[nums.length];
+
+        result[0] = 1;
+        //左前缀乘积
+        for (int i = 1; i < nums.length; i++) {
+            result[i] = result[i - 1] * nums[i - 1];
+        }
+
+        int right = 1;
+        //右前缀乘积
+        for (int i = nums.length - 1; i >= 0; i--) {
+            result[i] = result[i] * right;
+            right = right * nums[i];
+        }
+
+        return result;
+    }
+}
