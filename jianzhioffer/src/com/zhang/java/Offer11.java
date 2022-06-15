3e35119a18593ba7228338cafef81f5e7c2e0e26
zsy <275076723@qq.com>
2022-06-15 10:02:00 +0800

    617

--- jianzhioffer/src/com/zhang/java/Offer11.java
+++ jianzhioffer/src/com/zhang/java/Offer11.java
@@ -3,7 +3,8 @@ package com.zhang.java;
 /**
  * @Date 2022/3/14 15:59
  * @Author zsy
- * @Description 把一个数组最开始的若干个元素搬到数组的末尾，我们称之为数组的旋转。
+ * @Description 旋转数组的最小数字
+ * 把一个数组最开始的若干个元素搬到数组的末尾，我们称之为数组的旋转。
  * 给你一个可能存在重复元素值的数组numbers，它原来是一个升序排列的数组，并按上述情形进行了一次旋转。
  * 请返回旋转数组的最小元素。例如，数组[3,4,5,1,2] 为 [1,2,3,4,5] 的一次旋转，该数组的最小值为1。
  * <p>
@@ -16,32 +17,36 @@ package com.zhang.java;
 public class Offer11 {
     public static void main(String[] args) {
         Offer11 offer11 = new Offer11();
-        int[] numbers = {2,2,3,4,4,5,6,7,8,9,1,2};
+        int[] numbers = {2, 2, 3, 4, 4, 5, 6, 7, 8, 9, 1, 2};
         System.out.println(offer11.minArray(numbers));
     }
 
     /**
-     * 二分查找的变形，平均时间复杂度为O(logn)，最坏时间复杂度为O(n)，为所有元素都相同的情况
+     * 二分查找变形
+     * 平均时间复杂度为O(logn)，最坏时间复杂度为O(n)(所有元素都相同的情况)
+     *
      * @param numbers
      * @return
      */
     public int minArray(int[] numbers) {
-        int low = 0;
-        int high = numbers.length - 1;
+        int left = 0;
+        int right = numbers.length - 1;
         int mid;
-        while (low < high) {
-            mid = (low + high) >> 1;
-            if (numbers[mid] < numbers[high]) {
-                //mid值比high值小，说明右边有序，最小值不在右边
-                high = mid;
-            } else if (numbers[mid] > numbers[high]) {
-                //mid值比high值大，说明左边有序，最小值在右边
-                low = mid + 1;
+
+        while (left < right) {
+            mid = left + ((right - left) >> 1);
+            if (numbers[mid] < numbers[right]) {
+                //mid值比right值小，说明右边有序，最小值在左边或当前位置
+                right = mid;
+            } else if (numbers[mid] > numbers[right]) {
+                //mid值比right值大，说明左边有序，最小值在右边
+                left = mid + 1;
             } else {
-                //mid值等于high值，说明重复，去除high值
-                high--;
+                //mid值等于right值，说明重复，right左移一位
+                right--;
             }
         }
-        return numbers[low];
+
+        return numbers[left];
     }
 }
