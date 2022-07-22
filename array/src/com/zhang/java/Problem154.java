232f8dcee27d857aeb79e7a6349375206ca26e4b
zsy <275076723@qq.com>
2022-07-22 20:21:51 +0800

    154、232

---
+++ array/src/com/zhang/java/Problem154.java
@@ -0,0 +1,67 @@
+package com.zhang.java;
+
+/**
+ * @Date 2022/7/17 9:49
+ * @Author zsy
+ * @Description 寻找旋转排序数组中的最小值 II 类比Problem33、Problem34、Problem153、Problem162、Offer53 同Offer11
+ * 已知一个长度为 n 的数组，预先按照升序排列，经由 1 到 n 次 旋转 后，得到输入数组。
+ * 例如，原数组 nums = [0,1,4,4,5,6,7] 在变化后可能得到：
+ * 若旋转 4 次，则可以得到 [4,5,6,7,0,1,4]
+ * 若旋转 7 次，则可以得到 [0,1,4,4,5,6,7]
+ * 注意，数组 [a[0], a[1], a[2], ..., a[n-1]] 旋转一次 的结果为数组 [a[n-1], a[0], a[1], a[2], ..., a[n-2]] 。
+ * 给你一个可能存在 重复 元素值的数组 nums ，它原来是一个升序排列的数组，并按上述情形进行了多次旋转。
+ * 请你找出并返回数组中的 最小元素 。
+ * 你必须尽可能减少整个过程的操作步骤。
+ * <p>
+ * 输入：nums = [1,3,5]
+ * 输出：1
+ * <p>
+ * 输入：nums = [2,2,2,0,1]
+ * 输出：0
+ * <p>
+ * n == nums.length
+ * 1 <= n <= 5000
+ * -5000 <= nums[i] <= 5000
+ * nums 原来是一个升序排序的数组，并进行了 1 至 n 次旋转
+ */
+public class Problem154 {
+    public static void main(String[] args) {
+        Problem154 problem154 = new Problem154();
+        int[] nums = {2, 2, 2, 0, 1};
+        System.out.println(problem154.findMin(nums));
+    }
+
+    /**
+     * 二分查找变形，看到有序数组，就要想到二分查找
+     * 时间复杂度O(logn)，空间复杂度O(1)
+     *
+     * @param nums
+     * @return
+     */
+    public int findMin(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return -1;
+        }
+        if (nums.length == 1) {
+            return nums[0];
+        }
+
+        int left = 0;
+        int right = nums.length - 1;
+        int mid;
+
+        while (left < right) {
+            mid = left + ((right - left) >> 1);
+
+            if (nums[mid] > nums[right]) {
+                left = mid + 1;
+            } else if (nums[mid] < nums[right]) {
+                right = mid;
+            } else {
+                right--;
+            }
+        }
+
+        return nums[left];
+    }
+}
---
+++ stack/src/com/zhang/java/Problem232.java
@@ -0,0 +1,98 @@
+package com.zhang.java;
+
+import java.util.Stack;
+
+/**
+ * @Date 2022/7/19 8:09
+ * @Author zsy
+ * @Description 用栈实现队列 类比Problem155、Offer30 同Offer9
+ * 请你仅使用两个栈实现先入先出队列。队列应当支持一般队列支持的所有操作（push、pop、peek、empty）：
+ * 实现 MyQueue 类：
+ * void push(int x) 将元素 x 推到队列的末尾
+ * int pop() 从队列的开头移除并返回元素
+ * int peek() 返回队列开头的元素
+ * boolean empty() 如果队列为空，返回 true ；否则，返回 false
+ * 说明：
+ * 你 只能 使用标准的栈操作 —— 也就是只有 push to top, peek/pop from top, size, 和 is empty 操作是合法的。
+ * 你所使用的语言也许不支持栈。你可以使用 list 或者 deque（双端队列）来模拟一个栈，只要是标准的栈操作即可。
+ * <p>
+ * 输入：
+ * ["MyQueue", "push", "push", "peek", "pop", "empty"]
+ * [[], [1], [2], [], [], []]
+ * 输出：
+ * [null, null, null, 1, 1, false]
+ * 解释：
+ * MyQueue myQueue = new MyQueue();
+ * myQueue.push(1); // queue is: [1]
+ * myQueue.push(2); // queue is: [1, 2] (leftmost is front of the queue)
+ * myQueue.peek(); // return 1
+ * myQueue.pop(); // return 1, queue is [2]
+ * myQueue.empty(); // return false
+ * <p>
+ * 1 <= x <= 9
+ * 最多调用 100 次 push、pop、peek 和 empty
+ * 假设所有操作都是有效的 （例如，一个空的队列不会调用 pop 或者 peek 操作）
+ * <p>
+ * 你能否实现每个操作均摊时间复杂度为 O(1) 的队列？
+ * 换句话说，执行 n 个操作的总时间复杂度为 O(n) ，即使其中一个操作可能花费较长时间。
+ */
+public class Problem232 {
+    public static void main(String[] args) {
+        MyQueue myQueue = new MyQueue();
+        myQueue.push(1);
+        myQueue.push(2);
+        System.out.println(myQueue.peek());
+        System.out.println(myQueue.pop());
+        System.out.println(myQueue.empty());
+    }
+
+    private static class MyQueue {
+        private Stack<Integer> stack1;
+        private Stack<Integer> stack2;
+
+        public MyQueue() {
+            stack1 = new Stack<>();
+            stack2 = new Stack<>();
+        }
+
+        public void push(int x) {
+            stack1.push(x);
+        }
+
+        public int pop() {
+            if (!stack2.isEmpty()) {
+                return stack2.pop();
+            }
+
+            if (!stack1.isEmpty()) {
+                while (!stack1.isEmpty()) {
+                    stack2.push(stack1.pop());
+                }
+
+                return stack2.pop();
+            }
+
+            return -1;
+        }
+
+        public int peek() {
+            if (!stack2.isEmpty()) {
+                return stack2.peek();
+            }
+
+            if (!stack1.isEmpty()) {
+                while (!stack1.isEmpty()) {
+                    stack2.push(stack1.pop());
+                }
+
+                return stack2.peek();
+            }
+
+            return -1;
+        }
+
+        public boolean empty() {
+            return stack1.isEmpty() && stack2.isEmpty();
+        }
+    }
+}
