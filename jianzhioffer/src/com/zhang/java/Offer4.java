2c75f499f7f9d1a5453a985d7b90fa404e0bc0dc
zsy <275076723@qq.com>
2022-08-17 12:09:43 +0800

    378

--- jianzhioffer/src/com/zhang/java/Offer4.java
+++ jianzhioffer/src/com/zhang/java/Offer4.java
@@ -3,7 +3,7 @@ package com.zhang.java;
 /**
  * @Date 2022/3/13 11:26
  * @Author zsy
- * @Description 二维数组中的查找 类比Probelm74 同Problem240
+ * @Description 二维数组中的查找 类比Problem74、Problem378 同Problem240
  * 在一个 n * m 的二维数组中，每一行都按照从左到右递增的顺序排序，每一列都按照从上到下递增的顺序排序。
  * 请完成一个高效的函数，输入这样的一个二维数组和一个整数，判断数组中是否含有该整数
  * <p>
