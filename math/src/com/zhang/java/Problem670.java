99b6a7b6ede66da3f682ea7a559b7cb3a6cb79d7
zsy <275076723@qq.com>
2024-03-20 11:28:20 +0800

    1328

--- math/src/com/zhang/java/Problem670.java
+++ math/src/com/zhang/java/Problem670.java
@@ -3,7 +3,7 @@ package com.zhang.java;
 /**
  * @Date 2023/2/21 10:35
  * @Author zsy
- * @Description 最大交换 字节面试题 类比Problem31、Problem556、Problem738、Problem1323
+ * @Description 最大交换 字节面试题 类比Problem1328 类比Problem31、Problem556、Problem738、Problem1323
  * 给定一个非负整数，你至多可以交换一次数字中的任意两位。返回你能得到的最大值。
  * <p>
  * 输入: 2736
@@ -25,10 +25,8 @@ public class Problem670 {
 
     /**
      * 模拟
-     * 从前往后遍历，找第一个右边存在比当前元素值大的最大元素，如果存在相同的最大元素，则找最远的最大元素，
-     * 两者进行交换，得到交换一次得到的最大值
-     * 如果是最小交换，找第一个右边存在比当前元素值小的最小元素，如果存在相同的最小元素，则找最远的最小元素，
-     * 两者进行交换，得到交换一次得到的最小值
+     * 找比当前元素大且距离当前元素最远的元素，两者进行交换，得到交换一次得到的最大值
+     * 类比最小交换，找比当前元素小且距离当前元素最远的元素，两者进行交换，得到交换一次得到的最小值
      * 时间复杂度O((log(num))^2)=O(1)，空间复杂度O(log(num))=O(1)
      *
      * @param num
@@ -45,16 +43,14 @@ public class Problem670 {
         for (int i = 0; i < arr.length - 1; i++) {
             int index = i;
 
-            //找比arr[i]大的最大元素arr[index]，如果存在相同的最大元素，则找最远的最大元素，
-            //小于等于保证能够取到最远的最大元素
+            //找比arr[i]大且距离arr[i]最远的元素arr[index]，小于等于保证能够取到最远的最大元素
             for (int j = i + 1; j < arr.length; j++) {
                 if (arr[index] <= arr[j]) {
                     index = j;
                 }
             }
 
-            //找到了比arr[i]大的最大元素arr[index]，arr[index]是离arr[i]最远的最大元素，
-            //则arr[i]和arr[index]两者进行交换，得到交换一次得到的最大值
+            //找到比arr[i]大且距离arr[i]最远的元素arr[index]，则arr[i]和arr[index]进行交换，得到交换一次得到的最大值
             if (i != index && arr[i] < arr[index]) {
                 char temp = arr[i];
                 arr[i] = arr[index];
@@ -64,7 +60,7 @@ public class Problem670 {
             }
         }
 
-        //num递减，num就是最大值，不需要交换，直接返回num
+        //num递减，则num就是最大值，不需要交换，直接返回num
         return num;
     }
 }
