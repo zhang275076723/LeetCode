40c8562a395e1a9961e3e23ed453e5dd444435f8
zsy <275076723@qq.com>
2024-08-29 15:35:09 +0800

    2996

--- hot100/src/com/zhang/java/Problem128.java
+++ hot100/src/com/zhang/java/Problem128.java
@@ -6,7 +6,7 @@ import java.util.Set;
 /**
  * @Date 2022/5/3 10:06
  * @Author zsy
- * @Description 最长连续序列 字节面试题 类比Problem2150、Problem2154 类比Problem506、Problem539、Problem561、Problem628、Problem747 哈希表类比Problem1、Problem166、Problem187、Problem205、Problem242、Problem290、Problem291、Problem383、Problem387、Problem389、Problem454、Problem532、Problem535、Problem554、Problem609、Problem763、Problem1500、Problem1640、Problem2657、Offer50
+ * @Description 最长连续序列 字节面试题 类比Problem2150、Problem2154 类比Problem506、Problem539、Problem561、Problem628、Problem747、Problem1200、Problem1509、Problem2144 哈希表类比Problem1、Problem166、Problem187、Problem205、Problem242、Problem290、Problem291、Problem383、Problem387、Problem389、Problem454、Problem532、Problem535、Problem554、Problem609、Problem763、Problem1500、Problem1640、Problem2657、Offer50
  * 给定一个未排序的整数数组 nums ，找出数字连续的最长序列（不要求序列元素在原数组中连续）的长度。
  * 请你设计并实现时间复杂度为 O(n) 的算法解决此问题。
  * <p>
@@ -46,15 +46,17 @@ public class Problem128 {
         int curLen = 1;
 
         for (int i = 0; i < nums.length - 1; i++) {
-            //两数相等的情况
+            //nums[i]和nums[i+1]相等，直接进行下次循环
             if (nums[i] == nums[i + 1]) {
                 continue;
             }
 
+            //当前连续序列长度加1，更新maxLen
             if (nums[i] + 1 == nums[i + 1]) {
                 curLen++;
                 maxLen = Math.max(maxLen, curLen);
             } else {
+                //当前连续序列长度重新赋值为1
                 curLen = 1;
             }
         }
@@ -65,7 +67,7 @@ public class Problem128 {
 
     /**
      * 哈希表
-     * 核心思想：nums[i]是连续序列的第一个元素，才nums[i]开始找最长连续序列
+     * 核心思想：如果nums[i]是连续序列的第一个元素，则从nums[i]开始找最长连续序列
      * 将数组中所有元素加入哈希表，遍历数组判断nums[i]的前一个元素nums[i]-1是否在哈希表中：
      * 1、nums[i]-1在哈希表中，则可以等到遍历到nums[i]-1时，再找最长连续序列
      * 2、nums[i]-1不在哈希表中，则nums[i]作为当前连续序列的第一个元素，从nums[i]开始找最长连续序列
@@ -85,10 +87,10 @@ public class Problem128 {
             set.add(num);
         }
 
-        int maxLen = 0;
+        int maxLen = 1;
 
         for (int i = 0; i < nums.length; i++) {
-            //nums[i]的前一个元素nums[i]-1不在哈希表中，则nums[i]作为当前连续序列的第一个元素，从nums[i]开始找最长连续序列
+            //nums[i]-1不在哈希表中，nums[i]是连续序列的第一个元素，则从nums[i]开始找最长连续序列
             if (!set.contains(nums[i] - 1)) {
                 int curLen = 1;
                 int curNum = nums[i];
