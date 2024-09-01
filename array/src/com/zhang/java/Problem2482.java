5b7a0bc56b1d2248a53f95c8f077ab3955dbb786
zsy <275076723@qq.com>
2024-09-01 09:00:18 +0800

    2482、2661

---
+++ array/src/com/zhang/java/Problem2482.java
@@ -0,0 +1,93 @@
+package com.zhang.java;
+
+import java.util.Arrays;
+
+/**
+ * @Date 2024/12/20 08:02
+ * @Author zsy
+ * @Description 行和列中一和零的差值 类比Problem2133、Problem2661
+ * 给你一个下标从 0 开始的 m x n 二进制矩阵 grid 。
+ * 我们按照如下过程，定义一个下标从 0 开始的 m x n 差值矩阵 diff ：
+ * 令第 i 行一的数目为 onesRowi 。
+ * 令第 j 列一的数目为 onesColj 。
+ * 令第 i 行零的数目为 zerosRowi 。
+ * 令第 j 列零的数目为 zerosColj 。
+ * diff[i][j] = onesRowi + onesColj - zerosRowi - zerosColj
+ * 请你返回差值矩阵 diff 。
+ * <p>
+ * 输入：grid = [[0,1,1],[1,0,1],[0,0,1]]
+ * 输出：[[0,0,4],[0,0,4],[-2,-2,2]]
+ * 解释：
+ * - diff[0][0] = onesRow0 + onesCol0 - zerosRow0 - zerosCol0 = 2 + 1 - 1 - 2 = 0
+ * - diff[0][1] = onesRow0 + onesCol1 - zerosRow0 - zerosCol1 = 2 + 1 - 1 - 2 = 0
+ * - diff[0][2] = onesRow0 + onesCol2 - zerosRow0 - zerosCol2 = 2 + 3 - 1 - 0 = 4
+ * - diff[1][0] = onesRow1 + onesCol0 - zerosRow1 - zerosCol0 = 2 + 1 - 1 - 2 = 0
+ * - diff[1][1] = onesRow1 + onesCol1 - zerosRow1 - zerosCol1 = 2 + 1 - 1 - 2 = 0
+ * - diff[1][2] = onesRow1 + onesCol2 - zerosRow1 - zerosCol2 = 2 + 3 - 1 - 0 = 4
+ * - diff[2][0] = onesRow2 + onesCol0 - zerosRow2 - zerosCol0 = 1 + 1 - 2 - 2 = -2
+ * - diff[2][1] = onesRow2 + onesCol1 - zerosRow2 - zerosCol1 = 1 + 1 - 2 - 2 = -2
+ * - diff[2][2] = onesRow2 + onesCol2 - zerosRow2 - zerosCol2 = 1 + 3 - 2 - 0 = 2
+ * <p>
+ * 输入：grid = [[1,1,1],[1,1,1]]
+ * 输出：[[5,5,5],[5,5,5]]
+ * 解释：
+ * - diff[0][0] = onesRow0 + onesCol0 - zerosRow0 - zerosCol0 = 3 + 2 - 0 - 0 = 5
+ * - diff[0][1] = onesRow0 + onesCol1 - zerosRow0 - zerosCol1 = 3 + 2 - 0 - 0 = 5
+ * - diff[0][2] = onesRow0 + onesCol2 - zerosRow0 - zerosCol2 = 3 + 2 - 0 - 0 = 5
+ * - diff[1][0] = onesRow1 + onesCol0 - zerosRow1 - zerosCol0 = 3 + 2 - 0 - 0 = 5
+ * - diff[1][1] = onesRow1 + onesCol1 - zerosRow1 - zerosCol1 = 3 + 2 - 0 - 0 = 5
+ * - diff[1][2] = onesRow1 + onesCol2 - zerosRow1 - zerosCol2 = 3 + 2 - 0 - 0 = 5
+ * <p>
+ * m == grid.length
+ * n == grid[i].length
+ * 1 <= m, n <= 10^5
+ * 1 <= m * n <= 10^5
+ * grid[i][j] 要么是 0 ，要么是 1 。
+ */
+public class Problem2482 {
+    public static void main(String[] args) {
+        Problem2482 problem2482 = new Problem2482();
+        int[][] grid = {
+                {0, 1, 1},
+                {1, 0, 1},
+                {0, 0, 1}
+        };
+        System.out.println(Arrays.deepToString(problem2482.onesMinusZeros(grid)));
+    }
+
+    /**
+     * 模拟
+     * 时间复杂度O(mn)，空间复杂度O(m+n)
+     *
+     * @param grid
+     * @return
+     */
+    public int[][] onesMinusZeros(int[][] grid) {
+        int m = grid.length;
+        int n = grid[0].length;
+
+        //每行1的个数数组
+        int[] rowArr = new int[m];
+        //每列1的个数数组
+        int[] colArr = new int[n];
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (grid[i][j] == 1) {
+                    rowArr[i]++;
+                    colArr[j]++;
+                }
+            }
+        }
+
+        int[][] diff = new int[m][n];
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                diff[i][j] = rowArr[i] + colArr[j] - (m - rowArr[i]) - (n - colArr[j]);
+            }
+        }
+
+        return diff;
+    }
+}
---
+++ array/src/com/zhang/java/Problem2661.java
@@ -0,0 +1,94 @@
+package com.zhang.java;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @Date 2024/12/19 08:49
+ * @Author zsy
+ * @Description 找出叠涂元素 类比Problem2133、Problem2482
+ * 给你一个下标从 0 开始的整数数组 arr 和一个 m x n 的整数 矩阵 mat 。
+ * arr 和 mat 都包含范围 [1，m * n] 内的 所有 整数。
+ * 从下标 0 开始遍历 arr 中的每个下标 i ，并将包含整数 arr[i] 的 mat 单元格涂色。
+ * 请你找出 arr 中第一个使得 mat 的某一行或某一列都被涂色的元素，并返回其下标 i 。
+ * <p>
+ * 输入：arr = [1,3,4,2], mat = [[1,4],[2,3]]
+ * 输出：2
+ * 解释：遍历如上图所示，arr[2] 在矩阵中的第一行或第二列上都被涂色。
+ * <p>
+ * image explanation for example 2
+ * 输入：arr = [2,8,7,4,1,3,5,6,9], mat = [[3,2,5],[1,4,6],[8,7,9]]
+ * 输出：3
+ * 解释：遍历如上图所示，arr[3] 在矩阵中的第二列上都被涂色。
+ * <p>
+ * m == mat.length
+ * n = mat[i].length
+ * arr.length == m * n
+ * 1 <= m, n <= 10^5
+ * 1 <= m * n <= 10^5
+ * 1 <= arr[i], mat[r][c] <= m * n
+ * arr 中的所有整数 互不相同
+ * mat 中的所有整数 互不相同
+ */
+public class Problem2661 {
+    public static void main(String[] args) {
+        Problem2661 problem2661 = new Problem2661();
+        int[] arr = {2, 8, 7, 4, 1, 3, 5, 6, 9};
+        int[][] mat = {{3, 2, 5},
+                {1, 4, 6},
+                {8, 7, 9}
+        };
+        System.out.println(problem2661.firstCompleteIndex(arr, mat));
+    }
+
+    /**
+     * 模拟+哈希表
+     * 时间复杂度O(mn)，空间复杂度O(mn)
+     *
+     * @param arr
+     * @param mat
+     * @return
+     */
+    public int firstCompleteIndex(int[] arr, int[][] mat) {
+        int m = mat.length;
+        int n = mat[0].length;
+
+        //key：mat中元素，value：mat中元素的行列下标索引
+        Map<Integer, int[]> map = new HashMap<>();
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                map.put(mat[i][j], new int[]{i, j});
+            }
+        }
+
+        //每行涂色的元素个数数组
+        int[] rowArr = new int[m];
+        //每列涂色的元素个数数组
+        int[] colArr = new int[n];
+
+        for (int i = 0; i < arr.length; i++) {
+            int[] arr2 = map.get(arr[i]);
+            //arr[i]在mat中的行
+            int x = arr2[0];
+            //arr[i]在mat中的列
+            int y = arr2[1];
+
+            rowArr[x]++;
+
+            //第x行元素个数为n，则第x行都被涂色，返回i
+            if (rowArr[x] == n) {
+                return i;
+            }
+
+            colArr[y]++;
+
+            //第y列元素个数为m，则第y列都被涂色，返回i
+            if (colArr[y] == m) {
+                return i;
+            }
+        }
+
+        return -1;
+    }
+}
