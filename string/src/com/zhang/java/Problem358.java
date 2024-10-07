359d223b970e9289808beaa349a6402c64b2d0f5
zsy <275076723@qq.com>
2024-10-07 19:52:23 +0800

    3286

--- string/src/com/zhang/java/Problem358.java
+++ string/src/com/zhang/java/Problem358.java
@@ -39,10 +39,10 @@ public class Problem358 {
     }
 
     /**
-     * 优先队列，大根堆
-     * 统计s中字符出现的次数，将出现次数大于0的字符加入大根堆，当大根堆元素个数大于等于k个，依次从大根堆中出堆k个元素，
+     * 优先队列，大根堆+队列
+     * 统计s中字符出现的次数，将出现次数大于0的字符加入大根堆，当大根堆元素个数大于等于k个时，从大根堆中出堆k个元素，
      * 拼接这k个不相邻的字符，这k个字符出现次数减1，出现次数大于0的字符再重新加入大根堆，直至大根堆中元素个数小于k个，
-     * 大根堆剩余元素出堆，拼接出堆的元素，如果剩余元素的出现次数都为1，则得到结果字符串，否则，结果字符串不合法，返回""
+     * 大根堆剩余元素出堆，如果剩余元素出现次数大于1，则无法构成不相邻的字符串，直接返回""，否则拼接得到结果字符串
      * 时间复杂度O(n+|Σ|+nlog|Σ|)，空间复杂度O(|Σ|) (|Σ|=26，s中只包含小写字母)
      *
      * @param s
@@ -61,21 +61,22 @@ public class Problem358 {
             count[c - 'a']++;
         }
 
-        //大根堆，先按照字符出现次数由大到小排序，再按照字典序由小到大排序
-        PriorityQueue<Character> priorityQueue = new PriorityQueue<>(new Comparator<Character>() {
+        //优先队列，大根堆，存放字符出现的次数和字符
+        //按照字符出现的次数由大到小排序，再按照字典序由小到大排序
+        PriorityQueue<Pair> priorityQueue = new PriorityQueue<>(new Comparator<Pair>() {
             @Override
-            public int compare(Character c1, Character c2) {
-                if (count[c1 - 'a'] != count[c2 - 'a']) {
-                    return count[c2 - 'a'] - count[c1 - 'a'];
+            public int compare(Pair pair1, Pair pair2) {
+                if (pair1.count != pair2.count) {
+                    return pair2.count - pair1.count;
                 } else {
-                    return c1 - c2;
+                    return pair1.c - pair2.c;
                 }
             }
         });
 
         for (int i = 0; i < 26; i++) {
             if (count[i] > 0) {
-                priorityQueue.offer((char) ('a' + i));
+                priorityQueue.offer(new Pair((char) ('a' + i), count[i]));
             }
         }
 
@@ -83,38 +84,49 @@ public class Problem358 {
 
         //大根堆元素个数大于等于k，依次从大根堆中出堆k个元素拼接
         while (priorityQueue.size() >= k) {
-            //存储出堆的k个元素出现次数减1后大于0的字符队列
-            Queue<Character> queue = new LinkedList<>();
+            //存储出堆的k个元素的队列
+            Queue<Pair> queue = new LinkedList<>();
 
             //从大根堆中出堆k个元素的出现次数减1
             for (int i = 0; i < k; i++) {
-                char c = priorityQueue.poll();
-                sb.append(c);
-                count[c - 'a']--;
+                Pair pair = priorityQueue.poll();
+                sb.append(pair.c);
+                pair.count--;
 
                 //出堆的k个元素出现次数减1后大于0的字符入队
-                if (count[c - 'a'] > 0) {
-                    queue.offer(c);
+                if (pair.count > 0) {
+                    queue.offer(pair);
                 }
             }
 
-            //k个字符出现次数减1，出现次数大于0的字符再重新加入大根堆
+            //队列中出堆的k个元素出现次数大于0的字符再重新加入大根堆
             while (!queue.isEmpty()) {
                 priorityQueue.offer(queue.poll());
             }
         }
 
-        //大根堆剩余元素出堆，拼接出堆的元素
+        //大根堆剩余元素出堆，拼接得到结果字符串
         while (!priorityQueue.isEmpty()) {
-            int c = priorityQueue.poll();
-            sb.append(c);
+            Pair pair = priorityQueue.poll();
 
-            //剩余元素的出现次数大于1，则结果字符串不合法，返回""
-            if (count[c - 'a'] > 1) {
+            //当前字符出现次数大于1，则无法构成相同字符的位置间隔距离至少为k的字符串，返回""
+            if (pair.count > 1) {
                 return "";
             }
+
+            sb.append(pair.c);
         }
 
         return sb.toString();
+    }
+
+    private static class Pair {
+        private char c;
+        private int count;
+
+        public Pair(char c, int count) {
+            this.c = c;
+            this.count = count;
+        }
     }
 }
